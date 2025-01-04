b0dd608bee1e86008e75c01982f32ece276cf57f
odinokun <odinokun@gmail.com>
2025-01-04 14:05:29 -0500

    refactor: improve code formatting and readability in App and tasks-reducer components

--- src/App.tsx
+++ src/App.tsx
@@ -11,129 +11,129 @@ import { TaskType, Todolist } from './Todolist';
 import { Header } from './components/Header/Header';
 import { AddItemForm } from './components/AddItemForm/AddItemForm';
 import {
-	addTodolistAC,
-	changeTodolistFilterAC,
-	editTodolistNameAC,
-	removeTodolistAC,
-	todolistsReducer,
+  addTodolistAC,
+  changeTodolistFilterAC,
+  editTodolistNameAC,
+  removeTodolistAC,
+  todolistsReducer,
 } from './state/todolists-reducer';
 import { addTaskAC, changeStatusAC, editTaskAC, removeTaskAC, tasksReducer } from './state/tasks-reducer';
 
 export type TodolistType = {
-	id: string;
-	title: string;
-	filter: FilterType;
+  id: string;
+  title: string;
+  filter: FilterType;
 };
 export type AllTasksType = {
-	[key: string]: TaskType[];
+  [key: string]: TaskType[];
 };
 
 export type FilterType = 'all' | 'active' | 'completed';
 
 function App() {
-	const todolistsId1 = v1();
-	const todolistsId2 = v1();
+  const todolistsId1 = v1();
+  const todolistsId2 = v1();
 
-	const [todolists, dispatchTodolists] = useReducer(todolistsReducer, [
-		{ id: todolistsId1, title: 'To learn', filter: 'all' },
-		{ id: todolistsId2, title: 'Films', filter: 'active' },
-	]);
-	const [allTasks, dispatchTasks] = useReducer(tasksReducer, {
-		[todolistsId1]: [
-			{ id: v1(), title: 'HTML&CSS', isDone: true },
-			{ id: v1(), title: 'JS', isDone: true },
-			{ id: v1(), title: 'React', isDone: false },
-			{ id: v1(), title: 'Redux', isDone: false },
-			{ id: v1(), title: 'Rest API', isDone: false },
-			{ id: v1(), title: 'GraphQL', isDone: false },
-			{ id: v1(), title: 'Storybook', isDone: false },
-		],
-		[todolistsId2]: [
-			{ id: v1(), title: 'Friends', isDone: true },
-			{ id: v1(), title: 'Game of Thrones', isDone: true },
-			{ id: v1(), title: 'Peaky Blinders', isDone: false },
-			{ id: v1(), title: 'Breaking Bad', isDone: false },
-			{ id: v1(), title: 'The Witcher', isDone: false },
-		],
-	});
+  const [todolists, dispatchTodolists] = useReducer(todolistsReducer, [
+    { id: todolistsId1, title: 'To learn', filter: 'all' },
+    { id: todolistsId2, title: 'Films', filter: 'active' },
+  ]);
+  const [allTasks, dispatchTasks] = useReducer(tasksReducer, {
+    [todolistsId1]: [
+      { id: v1(), title: 'HTML&CSS', isDone: true },
+      { id: v1(), title: 'JS', isDone: true },
+      { id: v1(), title: 'React', isDone: false },
+      { id: v1(), title: 'Redux', isDone: false },
+      { id: v1(), title: 'Rest API', isDone: false },
+      { id: v1(), title: 'GraphQL', isDone: false },
+      { id: v1(), title: 'Storybook', isDone: false },
+    ],
+    [todolistsId2]: [
+      { id: v1(), title: 'Friends', isDone: true },
+      { id: v1(), title: 'Game of Thrones', isDone: true },
+      { id: v1(), title: 'Peaky Blinders', isDone: false },
+      { id: v1(), title: 'Breaking Bad', isDone: false },
+      { id: v1(), title: 'The Witcher', isDone: false },
+    ],
+  });
 
-	const deleteTodolist = (todolistId: string) => {
-		dispatchTodolists(removeTodolistAC(todolistId));
-		dispatchTasks(removeTodolistAC(todolistId));
-	};
+  const deleteTodolist = (todolistId: string) => {
+    dispatchTodolists(removeTodolistAC(todolistId));
+    dispatchTasks(removeTodolistAC(todolistId));
+  };
 
-	const addTodolist = (todolistTitle: string) => {
-		const action = addTodolistAC(todolistTitle);
-		dispatchTodolists(action);
-		dispatchTasks(action);
-	};
+  const addTodolist = (todolistTitle: string) => {
+    const action = addTodolistAC(todolistTitle);
+    dispatchTodolists(action);
+    dispatchTasks(action);
+  };
 
-	const editTodolistName = (todolistId: string, title: string) =>
-		dispatchTodolists(editTodolistNameAC(todolistId, title));
-	const changeFilter = (todolistId: string, filterVal: FilterType) =>
-		dispatchTodolists(changeTodolistFilterAC(todolistId, filterVal));
+  const editTodolistName = (todolistId: string, title: string) =>
+    dispatchTodolists(editTodolistNameAC(todolistId, title));
+  const changeFilter = (todolistId: string, filterVal: FilterType) =>
+    dispatchTodolists(changeTodolistFilterAC(todolistId, filterVal));
 
-	const addTask = (todolistId: string, title: string) => {
-		dispatchTasks(addTaskAC(todolistId, title));
-	};
-	const editTask = (todolistId: string, id: string, title: string) => dispatchTasks(editTaskAC(todolistId, id, title));
+  const addTask = (todolistId: string, title: string) => {
+    dispatchTasks(addTaskAC(todolistId, title));
+  };
+  const editTask = (todolistId: string, id: string, title: string) => dispatchTasks(editTaskAC(todolistId, id, title));
 
-	const removeTask = (todolistId: string, id: string) => dispatchTasks(removeTaskAC(todolistId, id));
+  const removeTask = (todolistId: string, id: string) => dispatchTasks(removeTaskAC(todolistId, id));
 
-	const changeStatus = (todolistId: string, id: string, isDone: boolean) =>
-		dispatchTasks(changeStatusAC(todolistId, id, isDone));
+  const changeStatus = (todolistId: string, id: string, isDone: boolean) =>
+    dispatchTasks(changeStatusAC(todolistId, id, isDone));
 
-	return (
-		<>
-			<Header />
+  return (
+    <>
+      <Header />
 
-			<Container maxWidth='lg'>
-				<Grid container spacing={3} sx={{ padding: '20px 0', alignItems: 'stretch' }}>
-					<Grid item xs={12}>
-						<Card variant='elevation' elevation={4} component='article' sx={{ height: '100%' }}>
-							<CardContent>
-								<Typography variant='h6' component='h2'>
-									Create new ToDo
-								</Typography>
-								<AddItemForm callback={addTodolist} errorText="Yo-yo!!! Where is the todolist's name!" />
-							</CardContent>
-						</Card>
-					</Grid>
+      <Container maxWidth='lg'>
+        <Grid container spacing={3} sx={{ padding: '20px 0', alignItems: 'stretch' }}>
+          <Grid item xs={12}>
+            <Card variant='elevation' elevation={4} component='article' sx={{ height: '100%' }}>
+              <CardContent>
+                <Typography variant='h6' component='h2'>
+                  Create new ToDo
+                </Typography>
+                <AddItemForm callback={addTodolist} errorText="Yo-yo!!! Where is the todolist's name!" />
+              </CardContent>
+            </Card>
+          </Grid>
 
-					{todolists.map(tl => {
-						const getFilteredTasks = () => {
-							switch (tl.filter) {
-								case 'active':
-									return allTasks[tl.id].filter((t: TaskType) => !t.isDone);
-								case 'completed':
-									return allTasks[tl.id].filter((t: TaskType) => t.isDone);
-								default:
-									return allTasks[tl.id];
-							}
-						};
-						const filteredTasks = getFilteredTasks();
+          {todolists.map(tl => {
+            const getFilteredTasks = () => {
+              switch (tl.filter) {
+                case 'active':
+                  return allTasks[tl.id].filter((t: TaskType) => !t.isDone);
+                case 'completed':
+                  return allTasks[tl.id].filter((t: TaskType) => t.isDone);
+                default:
+                  return allTasks[tl.id];
+              }
+            };
+            const filteredTasks = getFilteredTasks();
 
-						return (
-							<Todolist
-								key={tl.id}
-								todolistId={tl.id}
-								editTodolistName={editTodolistName}
-								title={tl.title}
-								tasks={filteredTasks}
-								removeTask={removeTask}
-								addTask={addTask}
-								editTask={editTask}
-								changeFilter={changeFilter}
-								changeTaskStatus={changeStatus}
-								filter={tl.filter}
-								deleteTodolist={deleteTodolist}
-							/>
-						);
-					})}
-				</Grid>
-			</Container>
-		</>
-	);
+            return (
+              <Todolist
+                key={tl.id}
+                todolistId={tl.id}
+                editTodolistName={editTodolistName}
+                title={tl.title}
+                tasks={filteredTasks}
+                removeTask={removeTask}
+                addTask={addTask}
+                editTask={editTask}
+                changeFilter={changeFilter}
+                changeTaskStatus={changeStatus}
+                filter={tl.filter}
+                deleteTodolist={deleteTodolist}
+              />
+            );
+          })}
+        </Grid>
+      </Container>
+    </>
+  );
 }
 
 export default App;
